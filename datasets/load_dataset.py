b8cb4261fa913db22ed1ed38ed9632e212e5c8cf
Nguyen Thanh Tung <tung.nguyent03@gmail.com>
2023-12-17 21:46:44 +0700

    finish labwork 3

--- datasets/load_dataset.py
+++ datasets/load_dataset.py
@@ -4,7 +4,7 @@ import os
 
 def iris(shuffle=False) -> tuple[pd.DataFrame, pd.DataFrame]:
   current = os.path.dirname(os.path.abspath(__file__))
-  data = pd.read_csv(current + "/iris_dataset/iris.data", header=None)
+  data = pd.read_csv(current + "/iris.data", header=None)
   feature_column = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
   if (shuffle):
     data = data.sample(frac=1).reset_index(drop=True)
@@ -14,7 +14,7 @@ def iris(shuffle=False) -> tuple[pd.DataFrame, pd.DataFrame]:
 
 def rice(shuffle=False) -> tuple[pd.DataFrame, pd.DataFrame]:
   current = os.path.dirname(os.path.abspath(__file__))
-  data = pd.read_csv(current + "/rice_dataset/Rice_Cammeo_Osmancik.csv", header=None)
+  data = pd.read_csv(current + "/Rice_Cammeo_Osmancik.csv", header=None)
   if (shuffle):
     data = data.sample(frac=1).reset_index(drop=True)
   feature_column = ['area', 'perimeter', 'compactness', 'length', 'width', 'asymmetry', 'groove_length']
@@ -22,9 +22,11 @@ def rice(shuffle=False) -> tuple[pd.DataFrame, pd.DataFrame]:
   
   return data, data[feature_column]
 
-def wine():
+def wine(shuffle=False):
   current = os.path.dirname(os.path.abspath(__file__))
-  data = pd.read_csv(current + "/wine_dataset/wine.csv", header=None)
+  data = pd.read_csv(current + "/wine.data", header=None)
+  if (shuffle):
+    data = data.sample(frac=1).reset_index(drop=True)
   feature_column = ["Alcohol", "Malic acid", "Ash", "Alcalinity of ash", "Magnesium", "Total phenols", "Flavanoids", "Nonflavanoid phenols", "Proanthocyanins", "Color intensity", "Hue", "OD280/OD315 of diluted wines", "Proline"]
   data.columns = ["class", *feature_column]
   return data, data[feature_column]
